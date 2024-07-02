30ad2f7f76f005c373f613e9eba79d37d7bbf5c8
vladostapovich <jorabumer@ukr.net>
2024-07-02 19:03:08 +0300

    fix

--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -2,7 +2,7 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
 axios.defaults.baseURL = "https://connections-api.goit.global/";
-https: axios.defaults.headers = { "Content-Type": "application/json" };
+axios.defaults.headers = { "Content-Type": "application/json" };
 
 const setAuthHeader = (token) =>
   (axios.defaults.headers["Authorization"] = token);
